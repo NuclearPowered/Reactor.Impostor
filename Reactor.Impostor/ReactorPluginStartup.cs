5dbaee025f1f5df8f90402ae9fe906d7c65c9dcd
js6pak <me@6pak.dev>
2023-11-10 11:57:57 +0100

    Update to Impostor 1.9

--- Reactor.Impostor/ReactorPluginStartup.cs
+++ Reactor.Impostor/ReactorPluginStartup.cs
@@ -1,17 +1,28 @@
 using Impostor.Api.Events;
+using Impostor.Api.Http;
 using Impostor.Api.Plugins;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Reactor.Impostor.Http;
 using Reactor.Impostor.Rpcs;
 
 namespace Reactor.Impostor;
 
-public class ReactorPluginStartup : IPluginStartup
+public class ReactorPluginStartup : IPluginHttpStartup
 {
     public void ConfigureServices(IServiceCollection services)
     {
         services.AddSingleton<IEventListener, HandshakeEventListener>();
         services.AddSingleton<IReactorCustomRpcManager, ReactorCustomRpcManager>();
+
+        services.AddSingleton<IListingFilter, ReactorHandshakeFilter>();
+        services.AddSingleton<ClientModsHeader>();
+    }
+
+    public void ConfigureWebApplication(IApplicationBuilder builder)
+    {
+        builder.UseMiddleware<ClientModsHeader>();
     }
 
     public void ConfigureHost(IHostBuilder host)
